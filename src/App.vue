ed57c5023c2d5e681ccbc8f373b6b0b3148c3697
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-05-31 14:04:59 -0300

    Create Warehouse Vue files

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/warehouses">Galpões Cadastrados</router-link> |
+      <router-link to="/warehouses/new">Cadastrar Galpão</router-link>
     </nav>
     <router-view/>
   </div>
---
+++ src/views/WarehouseNew.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Cadastrar Galpão</h1>
+    </div>
+</template>
