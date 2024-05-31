66b7f04cc50854fc4324198b69e1d5b05aa45f71
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-05-31 14:35:41 -0300

    Create Menu Vue file

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,27 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/warehouses">Galpões Cadastrados</router-link> |
-      <router-link to="/warehouses/new">Cadastrar Galpão</router-link>
-    </nav>
-    <router-view/>
-  </div>
+    <div id="app">
+        <Menu/>
+        <router-view/>
+    </div>
 </template>
+
+<script>
+import Menu from './components/Menu.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Menu
+    }
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <nav>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link> |
+            <router-link to="/warehouses">Galpões Cadastrados</router-link> |
+            <router-link to="/warehouses/new">Cadastrar Galpão</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Menu'
+}
+</script>
+
+
+<style>
+nav {
+    padding: 30px;
+}
+
+nav a {
+    font-weight: bold;
+    color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+    color: #42b983;
+}
+</style>
