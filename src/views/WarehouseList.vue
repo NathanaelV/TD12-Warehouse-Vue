56e6d82a6637c8ed15839ad7d0768decb12b32c3
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-06-01 10:34:54 -0300

    Show Warehouses

--- src/views/WarehouseList.vue
+++ src/views/WarehouseList.vue
@@ -1,12 +1,34 @@
 <template>
     <div>
         <h1>Galpões Cadastrados</h1>
+
+        <table border="1px">
+            <thead>
+                <th>Código</th>
+                <th>Nome</th>
+                <th>Sigla</th>
+            </thead>
+
+            <tbody>
+                <tr v-for="w in warehouses" :key="w.code">
+                    <td>{{ w.id }}</td>
+                    <td>{{ w.name }}</td>
+                    <td>{{ w.code}}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
 export default {
     name: 'WarehouseList',
+
+    data() {
+        return {
+            warehouses: []
+        }
+    },
 
     async mounted() {
         this.getWarehouses();
@@ -21,7 +43,9 @@ export default {
             // Extrair o JSON
             const result = await response.json();
 
-            console.log(result);
+            // Adicionar os dados de result no Array warehouses:
+            this.warehouses = result;
+            // return this.warehouses
         }
     },
 }
