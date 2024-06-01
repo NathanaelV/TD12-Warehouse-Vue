928dd65f56304b12d3d19b78201dc5773156536b
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-06-01 08:38:50 -0300

    Get Warehouses by API

--- src/views/WarehouseList.vue
+++ src/views/WarehouseList.vue
@@ -6,7 +6,24 @@
 
 <script>
 export default {
-    name: 'WarehouseList'
+    name: 'WarehouseList',
+
+    async mounted() {
+        this.getWarehouses();
+    },
+
+    methods: {
+        async getWarehouses() {
+
+            // Realizar a requisição
+            const response = await this.$http.get('http://localhost:3000/api/v1/warehouses');
+
+            // Extrair o JSON
+            const result = await response.json();
+
+            console.log(result);
+        }
+    },
 }
 </script>
 
